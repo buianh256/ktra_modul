d72a881ebae15c613c7424e4b36a9e53d72f523b
buianh256 <ducanhlccd1@example.com>
2023-10-05 11:01:47 +0700

    bai ktra thuc hanh

---
+++ bai4/rectangle.js
@@ -0,0 +1,23 @@
+class Rectangle{
+    x;
+    y;
+    width;
+    height;
+    color;
+    ctx;
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.ctx = document.getElementById("canvas").getContext("2d");
+    }
+    draw(){
+        this.ctx.beginPath();
+        this.ctx.rect(this.x, this.y, this.width, this.height);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+}
